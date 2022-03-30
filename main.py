19a38ecf0a9131dab25c7d9b652658c019fed81e
imPDA <dimaxa7@mail.ru>
2022-03-30 12:25:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if any(substring in message.content.lower() for substring in ['пария', 'парайа', 'париа', 'paria']):
+      stuhn_link = "https://cdn.discordapp.com/attachments/953278284098076672/958658012845838346/pariah.png"
+      await message.channel.send(stuhn_link)
+
+if __name__ == '__main__':
+    client.run(os.environ['TOKEN'])
